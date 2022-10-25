813ab5eeafff1a1a926e3db3b35d26f297bd3957
user <alehandr.romaniuk@gmail.com>
2022-10-25 21:57:29 +0300

    Create microservices architecture

---
+++ FunctionalMicrosrvice/src/main/java/com/example/Microservices/exeptions/RegistrationException.java
@@ -0,0 +1,8 @@
+package com.example.Microservices.exeptions;
+
+public class RegistrationException extends RuntimeException {
+
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
