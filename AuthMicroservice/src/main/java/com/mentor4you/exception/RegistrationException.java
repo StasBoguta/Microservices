b10dd6b35e40b56998c1621e06491ddbd23ee919
Victor <slavco2.p@gmail.com>
2022-11-07 00:41:06 +0200

    Prepare for merge

--- AuthMicroservice/src/main/java/com/mentor4you/exception/RegistrationException.java
+++
@@ -1,8 +0,0 @@
-package com.mentor4you.exception;
-
-public class RegistrationException extends RuntimeException{
-
-    public RegistrationException(String message) {
-        super(message);
-    }
-}
