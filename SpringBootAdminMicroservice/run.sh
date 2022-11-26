54abc36659381712cbe5a42b9d46060eac917091
Victor <slavco2.p@gmail.com>
2022-11-26 14:09:51 +0200

    Add prometheus and grafana

--- SpringBootAdminMicroservice/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "********************************************************"
-echo "Starting backend app"
-echo "********************************************************"
-
-java -Dserver.port=$SERVER_PORT \
-     -jar /app/backend.jar
